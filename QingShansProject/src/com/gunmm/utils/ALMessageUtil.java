c7c84e57b95e337abf64dfab919f8c2cfcf5a63f
gunmm <924744097@qq.com>
2018-11-11 16:53:55 +0800

    --

--- QingShansProject/src/com/gunmm/utils/ALMessageUtil.java
+++ QingShansProject/src/com/gunmm/utils/ALMessageUtil.java
@@ -24,8 +24,8 @@ public class ALMessageUtil {
 			final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
 
 			// 替换成你的AK
-			final String accessKeyId = "LTAILLEPyq9qdZ1s";// 你的accessKeyId,参考本文档步骤2
-			final String accessKeySecret = "gK1JOousSV5LQZdcq14hvkZx7nRFOi";// 你的accessKeySecret，参考本文档步骤2
+			final String accessKeyId = "";// 你的accessKeyId,参考本文档步骤2
+			final String accessKeySecret = "";// 你的accessKeySecret，参考本文档步骤2
 			// 初始化ascClient,暂时不支持多region（请勿修改）
 			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
 			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
