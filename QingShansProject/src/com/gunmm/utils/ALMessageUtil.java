374899414833178ad43eaa1cef1716643c431993
gunmm <924744097@qq.com>
2018-11-11 20:31:05 +0800

    遗漏文件

--- QingShansProject/src/com/gunmm/utils/ALMessageUtil.java
+++ QingShansProject/src/com/gunmm/utils/ALMessageUtil.java
@@ -24,8 +24,8 @@ public class ALMessageUtil {
 			final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
 
 			// 替换成你的AK
-			final String accessKeyId = "";// 你的accessKeyId,参考本文档步骤2
-			final String accessKeySecret = "";// 你的accessKeySecret，参考本文档步骤2
+			final String accessKeyId = "LTAILLEPyq9qdZ1s";// 你的accessKeyId,参考本文档步骤2
+			final String accessKeySecret = "gK1JOousSV5LQZdcq14hvkZx7nRFOi";// 你的accessKeySecret，参考本文档步骤2
 			// 初始化ascClient,暂时不支持多region（请勿修改）
 			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
 			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
@@ -129,6 +129,46 @@ public class ALMessageUtil {
 		request.setSignName("庆山物流");
 		// 必填:短信模板-可在短信控制台中找到，发送国际/港澳台消息时，请使用国际/港澳台短信模版
 		request.setTemplateCode("SMS_150742008");
+		// 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+		// 友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("code", codeString);
+
+		request.setTemplateParam(jsonObject.toString());
+		// 可选-上行短信扩展码(扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段)
+		// request.setSmsUpExtendCode("90997");
+		// 可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+		request.setOutId("yourOutId");
+		// 请求失败这里会抛ClientException异常
+		SendSmsResponse sendSmsResponse;
+		try {
+			sendSmsResponse = ALMessageUtil.getIAcsClient().getAcsResponse(request);
+			if (sendSmsResponse.getCode() != null && sendSmsResponse.getCode().equals("OK")) {
+				// 请求成功
+				return true;
+			} else {
+				return false;
+			}
+		} catch (ClientException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+	
+	public boolean sendBindSmallDriverMsg(String phoneNumber, String codeString) {
+
+		// 组装请求对象
+		SendSmsRequest request = new SendSmsRequest();
+		// 使用post提交
+		request.setMethod(MethodType.POST);
+		// 必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式；发送国际/港澳台消息时，接收号码格式为国际区号+号码，如“85200000000”
+		request.setPhoneNumbers(phoneNumber);
+		// 必填:短信签名-可在短信控制台中找到
+		request.setSignName("庆山物流");
+		// 必填:短信模板-可在短信控制台中找到，发送国际/港澳台消息时，请使用国际/港澳台短信模版
+		request.setTemplateCode("SMS_150742259");
 		// 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
 		// 友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
 		JSONObject jsonObject = new JSONObject();
